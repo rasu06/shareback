e5c5193886e295adcfaac2da2202ba410e1ce78a
unknown <rasubisen06@gmail.com>
2021-04-26 14:50:36 +0530

    'new'

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 // Cors 
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
-  ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
 // Default configuration looks like
@@ -18,31 +18,21 @@ const corsOptions = {
 //     "optionsSuccessStatus": 204
 //   }
 
-
-app.use(cors(corsOptions));
-
+app.use(cors(corsOptions))
 app.use(express.static('public'));
-
-
 
 const connectDB = require('./config/db');
 connectDB();
 
-
-app.use(express.json());  
-
-
+app.use(express.json());
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-
+// Routes 
 app.use('/api/files', require('./routes/files'));
-
 app.use('/files', require('./routes/show'));
-
 app.use('/files/download', require('./routes/download'));
-app.get('/',(req,res)=>{res.send("hello there")});
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
